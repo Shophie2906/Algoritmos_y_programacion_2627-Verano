310c0fe3771ede614726d2ac3da3f928035c3631
Shophie2906 <sanchez.elenas2906@gmail.com>
2026-08-01 13:25:43 -0400

    changes and quizz

---
+++ Quizz_1/db.py
@@ -0,0 +1,178 @@
+
+db={
+  "categorias": [
+    {
+      "nombre": "Mariscos",
+      "id": 1,
+      "cantidad_productos": 3,
+      "id_encargado": 101,
+      "productos": [3, 7, 10]
+    },
+    {
+      "nombre": "Bebidas alcohólicas",
+      "id": 2,
+      "cantidad_productos": 2,
+      "id_encargado": 102,
+      "productos": [5, 8]
+    },
+    {
+      "nombre": "Pastas",
+      "id": 3,
+      "cantidad_productos": 2,
+      "id_encargado": 103,
+      "productos": [1, 6]
+    },
+    {
+      "nombre": "Comidas para niños",
+      "id": 4,
+      "cantidad_productos": 2,
+      "id_encargado": 104,
+      "productos": [2, 9]
+    },
+    {
+      "nombre": "Bebidas no alcohólicas",
+      "id": 5,
+      "cantidad_productos": 2,
+      "id_encargado": 105,
+      "productos": [4, 11]
+    }
+  ],
+  "productos": [
+    {
+      "id": 1,
+      "nombre": "Espaguetis a la boloñesa",
+      "tipo": "alimento",	
+      "precio": 10.99,
+      "cantidad_stock": 15,
+      "porcion": "400g",
+      "personas": 1
+    },
+    {
+      "id": 2,
+      "nombre": "Nuggets de pollo",
+      "tipo": "alimento",
+      "precio": 5.99,
+      "cantidad_stock": 12,
+      "porcion": "150g",
+      "personas": 1
+    },
+    {
+      "id": 3,
+      "nombre": "Pulpo a la gallega",
+      "tipo": "alimento",	
+      "precio": 15.99,
+      "cantidad_stock": 8,
+      "porcion": "300g",
+      "personas": 2
+    },
+    {
+      "id": 4,
+      "nombre": "Jugo de naranja",
+      "tipo": "bebida",	
+      "precio": 3.99,
+      "cantidad_stock": 18,
+      "alcoholica": False,
+      "volumen": 0.5
+    },
+    {
+      "id": 5,
+      "nombre": "Cerveza artesanal",
+      "tipo": "bebida",	
+      "precio": 4.99,
+      "cantidad_stock": 20,
+      "alcoholica": True,
+      "volumen": 0.33
+    },
+    {
+      "id": 6,
+      "nombre": "Lasaña de carne",
+      "tipo": "alimento",	
+      "precio": 14.99,
+      "cantidad_stock": 7,
+      "porcion": "500g",
+      "personas": 2
+    },
+    {
+      "id": 7,
+      "nombre": "Ceviche de camarón",
+      "tipo": "alimento",	
+      "precio": 12.99,
+      "cantidad_stock": 10,
+      "porcion": "250g",
+      "personas": 1
+    },
+    {
+      "id": 8,
+      "nombre": "Vino tinto reserva",
+      "tipo": "bebida",	
+      "precio": 25.99,
+      "cantidad_stock": 5,
+      "alcoholica": True,
+      "volumen": 0.75
+    },
+    {
+      "id": 9,
+      "nombre": "Mini hamburguesa",
+      "tipo": "alimento",	
+      "precio": 6.99,
+      "cantidad_stock": 10,
+      "porcion": "200g",
+      "personas": 1
+    },
+    {
+      "id": 10,
+      "nombre": "Langostinos al ajillo",
+      "tipo": "alimento",	
+      "precio": 18.99,
+      "cantidad_stock": 6,
+      "porcion": "350g",
+      "personas": 3
+    },
+    {
+      "id": 11,
+      "nombre": "Refresco de cola",
+      "tipo": "bebida",	
+      "precio": 2.49,
+      "cantidad_stock": 20,
+      "alcoholica": False,
+      "volumen": 0.35
+    }
+  ],
+  "trabajadores": [
+    {
+      "id": 101,
+      "nombre": "Carlos Méndez",
+      "cargo": "Encargado de mariscos",
+      "edad": 45,
+      "horas_trabajo_diarias": 8
+    },
+    {
+      "id": 102,
+      "nombre": "Luis Fernández",
+      "cargo": "Encargado de bebidas alcohólicas",
+      "edad": 38,
+      "horas_trabajo_diarias": 7
+    },
+    {
+      "id": 103,
+      "nombre": "María García",
+      "cargo": "Encargada de pastas",
+      "edad": 34,
+      "horas_trabajo_diarias": 8
+    },
+    {
+      "id": 104,
+      "nombre": "Ana Pérez",
+      "cargo": "Encargada de comidas para niños",
+      "edad": 29,
+      "horas_trabajo_diarias": 6
+    },
+    {
+      "id": 105,
+      "nombre": "Juan López",
+      "cargo": "Encargado de bebidas no alcohólicas",
+      "edad": 41,
+      "horas_trabajo_diarias": 7
+    }
+  ]
+}
